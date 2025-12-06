1b3e598fcfa0e6ed67c0ed93277ded146e0d4b01
haegon123 <haegon@yahoo.com>
2025-12-06 15:04:42 -0500

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+{
+  "name": "steemit-scheduler-backend",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "pg": "^8.11.3",
+    "steem": "^0.7.18",
+    "node-cron": "^3.0.3",
+    "bcrypt": "^5.1.1",
+    "express-session": "^1.18.0",
+    "body-parser": "^1.20.2",
+    "crypto-js": "^4.2.0",
+    "uuid": "^9.0.1"
+  }
+}
